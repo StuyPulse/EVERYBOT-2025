2e4c8da3367cc40ccbae24102a5f2af90a3aa89d
dfn-slxxp <seb@sebastianw.tech>
2025-03-14 23:52:39 -0400

    gave myself credit in the auton

--- src/main/java/com/stuypulse/robot/commands/auton/DoNothingAuton.java
+++ src/main/java/com/stuypulse/robot/commands/auton/DoNothingAuton.java
@@ -15,7 +15,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
  * This auton does nothing... it is used as a placeholder
  *
  * @author Sam Belliveau
- * 
+ * edited by sebastian waldman
  */
 public class DoNothingAuton extends SequentialCommandGroup {
 
