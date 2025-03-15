94f94eb4b218390ddadbd8cfb107e4dde7013b95
dfn-slxxp <seb@sebastianw.tech>
2025-03-14 23:43:16 -0400

    hopefully added an L1 auton(one L1) really hope this works

---
+++ src/main/java/com/stuypulse/robot/commands/auton/OneL1Auton.java
@@ -0,0 +1,32 @@
+/************************ PROJECT PHIL ************************/
+/* Copyright (c) 2024 StuyPulse Robotics. All rights reserved.*/
+/* This work is licensed under the terms of the MIT license.  */
+/**************************************************************/
+
+package com.stuypulse.robot.commands.auton;
+
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import com.stuypulse.robot.commands.drive.DriveTank;
+import com.stuypulse.robot.commands.pivot.PivotCoralOut;
+import com.stuypulse.robot.subsystems.drivetrain.DrivetrainImpl;
+
+
+/*
+ * Move for RP; thats it bruh
+ *
+ * @author Sebastian Waldman
+ * I hope this works first try :pray:
+ */
+public class OneL1Auton extends SequentialCommandGroup {
+
+    public OneL1Auton() {
+        addCommands(
+            new DriveTank(DrivetrainImpl.getInstance(), .25, .25, true),
+            new WaitUntilCommand(10.00),
+            new DriveTank(DrivetrainImpl.getInstance(), 0, 0, true),
+            new PivotCoralOut()
+        );
+    }
+}
