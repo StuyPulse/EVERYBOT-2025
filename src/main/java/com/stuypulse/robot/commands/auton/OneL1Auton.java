cc92fdd6d1d0920074019f5e6a8fd7ce0722aa04
dfn-slxxp <seb@sebastianw.tech>
2025-03-16 23:13:18 -0400

    moved auton color settings into settings.java

--- src/main/java/com/stuypulse/robot/commands/auton/OneL1Auton.java
+++ src/main/java/com/stuypulse/robot/commands/auton/OneL1Auton.java
@@ -6,13 +6,12 @@
 package com.stuypulse.robot.commands.auton;
 
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-import edu.wpi.first.units.Units;
-import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import com.stuypulse.robot.commands.drive.DriveTank;
 import com.stuypulse.robot.commands.leds.LEDApplyPattern;
 import com.stuypulse.robot.commands.pivot.PivotCoralOut;
+import com.stuypulse.robot.constants.Settings.LEDPatterns;
 
 
 /*
@@ -25,7 +24,7 @@ public class OneL1Auton extends SequentialCommandGroup {
 
     public OneL1Auton() {
         addCommands(
-            new LEDApplyPattern(LEDPattern.rainbow(250,250).blink(Units.Seconds.of(1.5))),
+            new LEDApplyPattern(LEDPatterns.SINGLE_L1_AUTON),
             new DriveTank(.25, .25, true),
             new WaitUntilCommand(5.00),
             new DriveTank(0, 0, true),
