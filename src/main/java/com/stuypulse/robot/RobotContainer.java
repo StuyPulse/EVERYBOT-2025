cde75bb0908e22407b9e0d98d44fe866f1601042
Sebastian <63021947+dfn-slxxp@users.noreply.github.com>
2025-03-23 18:47:52 -0400

    fix error(lucas forgot a semicolon)

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -75,7 +75,7 @@ public class RobotContainer {
         if(Settings.DriveMode.GAMEPAD.toString() == "XBOX") {
             driver.getTopButton()
                 .whileTrue(new PivotCoralOut())
-                .whileTrue(new LEDApplyPattern(Settings.LEDPatterns.CORAL_OUT))
+                .whileTrue(new LEDApplyPattern(Settings.LEDPatterns.CORAL_OUT));
             driver.getLeftButton()
                 .onTrue(new ClimbToClimb());
             driver.getRightButton()
@@ -98,7 +98,8 @@ public class RobotContainer {
         else if(Settings.DriveMode.GAMEPAD.toString() == "JOYSTICK") {     
             joystick.getTriggerTriggered()
                 .whileTrue(new PivotCoralOut());
-        
+                .whileTrue(new LEDApplyPattern(Settings.LEDPatterns.CORAL_OUT));
+            
             joystick.getTop_TopRightButton()
                 .onTrue(new ClimbToStow());
             joystick.getTop_TopLeftButton()
