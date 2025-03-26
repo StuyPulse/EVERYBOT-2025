abc9574106ef8b856a8f12ce2726205e756b7e1e
Lucas <lucas.cs.programming@gmail.com>
2025-03-26 18:46:50 -0400

    update CAN Ds given from mec

--- src/main/java/com/stuypulse/robot/constants/Ports.java
+++ src/main/java/com/stuypulse/robot/constants/Ports.java
@@ -7,12 +7,12 @@ package com.stuypulse.robot.constants;
 
 /** This file contains the different ports of motors, solenoids and sensors */
 public interface Ports {
-    // TODO: Await port numbers from mec, put them here
+    // TODO: Await CAN IDs / Device IDs from mec, put them here
 
     public interface Gamepad {
         int DRIVER = 0;
         int OPERATOR = 1;
-        int JOYSTICK = 3; // Should this be of another port?
+        // int JOYSTICK = 3; // Should this be of another port?
         int DEBUGGER = 2;
     }
 
@@ -22,20 +22,20 @@ public interface Ports {
     }
       
     public interface Climb {
-        int CLIMB_MOTOR = 12;    
-        int CLIMB_ENCODER = 12;
+        int CLIMB_MOTOR = 2;
+        int CLIMB_ENCODER = 2;
     }
 
     public interface Pivot {
-        int PIVOT_MOTOR = 13;
-        int ROLLER_MOTOR = 14;
+        int PIVOT_MOTOR = 1;
+        int ROLLER_MOTOR = 3;
     }
   
     public interface Drivetrain {
-        public static final int LEFT_LEAD = 1; 
-        public static final int LEFT_FOLLOW = 0;
+        public static final int LEFT_LEAD = 5; 
+        public static final int LEFT_FOLLOW = 4;
         
-        public static final int RIGHT_LEAD = 3;
-        public static final int RIGHT_FOLLOW = 2;
+        public static final int RIGHT_LEAD = 7;
+        public static final int RIGHT_FOLLOW = 6;
     }
 }
