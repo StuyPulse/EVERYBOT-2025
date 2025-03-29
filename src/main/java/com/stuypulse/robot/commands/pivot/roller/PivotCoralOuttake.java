67b75fb5f6546329f622f526b4094fef458e022b
dfn-slxxp <waldman.sebastian@gmail.com>
2025-03-29 17:47:59 -0400

    rotisseriwesdjkfhgsdihj

--- src/main/java/com/stuypulse/robot/commands/pivot/roller/PivotCoralOuttake.java
+++ src/main/java/com/stuypulse/robot/commands/pivot/roller/PivotCoralOuttake.java
@@ -2,6 +2,6 @@ package com.stuypulse.robot.commands.pivot.roller;
 import com.stuypulse.robot.constants.Settings;
 public class PivotCoralOuttake extends PivotRollerToDirection {
     public PivotCoralOuttake() {
-        super(Settings.Pivot.ROLLER_STOP_SPEED.getAsDouble());
+        super(Settings.Pivot.CORAL_SHOOT_SPEED.getAsDouble());
     }
 }
