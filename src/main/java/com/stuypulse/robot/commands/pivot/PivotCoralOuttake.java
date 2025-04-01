eda1cf7ea0de45c54c992351abd22f0c432b0b16
dfn-slxxp <waldman.sebastian@gmail.com>
2025-03-31 20:59:51 -0400

    lower pivot when outtaking coral to prevent backwash

---
+++ src/main/java/com/stuypulse/robot/commands/pivot/PivotCoralOuttake.java
@@ -0,0 +1,20 @@
+package com.stuypulse.robot.commands.pivot;
+import com.stuypulse.robot.commands.leds.LEDApplyPattern;
+import com.stuypulse.robot.commands.pivot.roller.PivotRollerToDirection;
+import com.stuypulse.robot.constants.Settings;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+
+public class PivotCoralOuttake extends SequentialCommandGroup {
+    public PivotCoralOuttake() {
+        addCommands(
+            new LEDApplyPattern(Settings.LEDPatterns.CORAL_OUT),
+            new PivotRollerToDirection(Settings.Pivot.CORAL_SHOOT_SPEED.getAsDouble()),
+            new PivotToDirection(Settings.Pivot.PIVOT_LOWER_SPEED.getAsDouble()),
+            new WaitUntilCommand(1.5),
+            new PivotRollerToDirection(0),
+            new PivotToDirection(Settings.Pivot.PIVOT_RAISE_SPEED.getAsDouble())
+        );
+    }
+}
