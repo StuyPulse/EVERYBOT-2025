ddcd2292c52639b63ceb39176dfa6143ddade6f1
Danx3mer <dashingdaniel05@gmail.com>
2025-04-23 21:35:45 -0400

    Fix import issues w/ the change in package capitalization, fix some formatting

--- src/main/java/com/stuypulse/robot/commands/pivot/SetPivotControlMode.java
+++ src/main/java/com/stuypulse/robot/commands/pivot/SetPivotControlMode.java
@@ -8,8 +8,8 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 public class SetPivotControlMode extends InstantCommand {
     Pivot pivot = Pivot.getInstance();
     PivotControlMode pivotControlMode;
-    
-    public SetPivotControlMode (PivotControlMode pivotControlMode) {
+
+    public SetPivotControlMode(PivotControlMode pivotControlMode) {
         this.pivotControlMode = pivotControlMode;
     }
 
