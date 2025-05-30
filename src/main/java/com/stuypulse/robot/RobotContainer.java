cae85c030ba6463ca217b401247bae4bc98e3ddd
dfn-slxxp <waldman.sebastian@gmail.com>
2025-05-30 16:23:10 -0400

    working on autons - the chairmen

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -170,7 +170,6 @@ public class RobotContainer {
 		// PATHPLANNER
 		driveSubsystem.configureAutoBuilder();
 		registerAutoCommands();
-		// registerEventTriggers();
 
 		autonChooser.addOption("PP Coral 1PC", new PathPlannerAuto("Center 1Pc"));
 		autonChooser.addOption("pp 2 peice", new PathPlannerAuto("Non-Processor 2 Pc + AlgaePickup"));
@@ -186,20 +185,12 @@ public class RobotContainer {
 	private void registerAutoCommands() {
 		NamedCommands.registerCommand("PivotCoralScore",
 				new SequentialCommandGroup(
-						new PivotCoralScore().withTimeout(1), new PivotToCoralStow(), new PivotRollerStop()));
+						new PivotCoralScore().withTimeout(1), new PivotToCoralStow().withTimeout(.02), new PivotRollerStop().withTimeout(0.02)));
 		NamedCommands.registerCommand("PivotLollipopAlgaeIntake", new PivotLolipopAlgeaIntake());
 		NamedCommands.registerCommand("PivotAlgaeHold",
 				new SequentialCommandGroup(new PivotAlgaeHold(), new PivotToAlgaeStow()));
 		NamedCommands.registerCommand("PivotAlgaeOuttake", new PivotAlgaeOuttake());
 	}
-
-	// private void registerEventTriggers() {
-	// new EventTrigger("Coral - Score").onTrue(new
-	// SetPivotControlMode(PivotControlMode.USING_STATES))
-	// .whileTrue(new PivotCoralScore())
-	// .onFalse(new PivotToCoralStow())
-	// .onFalse(new PivotHoldCoral());
-	// }
 
 	public void configureSysId() {
 		SysIdRoutine pivotSysIdRoutine = pivot.getSysIdRoutine();
