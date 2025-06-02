78e584aac7af085876e5eb2146e72b6b8430f0da
Apetrock24 <yunusgubrud@gmail.com>
2025-06-02 16:52:14 -0400

    fixing imports

--- src/main/java/com/stuypulse/robot/commands/pivot/pivotCombos/PivotLollipopAlgaeIntake.java
+++ src/main/java/com/stuypulse/robot/commands/pivot/pivotCombos/PivotLollipopAlgaeIntake.java
@@ -1,6 +1,6 @@
 package com.stuypulse.robot.commands.pivot.pivotCombos;
 
-import com.stuypulse.robot.commands.pivot.PivotToLoliPopIntake;
+import com.stuypulse.robot.commands.pivot.PivotToLollipopIntake;
 import com.stuypulse.robot.commands.pivot.SetPivotControlMode;
 import com.stuypulse.robot.commands.pivot.roller.PivotAlgaeIntake;
 import com.stuypulse.robot.subsystems.pivot.Pivot;
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class PivotLollipopAlgaeIntake extends SequentialCommandGroup {
     public PivotLollipopAlgaeIntake() {
         addCommands(
-            new PivotToLoliPopIntake()
+            new PivotToLollipopIntake()
                 .withTimeout(2),
             new PivotAlgaeIntake()
         );
