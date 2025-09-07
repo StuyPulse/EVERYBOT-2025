bfa05b8b02f4180ed6c1d21b4ebdd3c573053e57
Daniel Opara <opara88daniel@gmail.com>
2025-09-07 19:52:33 -0400

    Removed phoenix 5 from Motors.java

--- src/main/java/com/stuypulse/robot/constants/Motors.java
+++ src/main/java/com/stuypulse/robot/constants/Motors.java
@@ -4,12 +4,13 @@
 /**************************************************************/
 package com.stuypulse.robot.constants;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+// import com.ctre.phoenix.motorcontrol.NeutralMode;
+// import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+// import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+// import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.spark.config.SparkBaseConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 /**
  * File containing all of the configurations that different motors require.
@@ -35,64 +36,64 @@ public interface Motors {
     }
 
     /** Classes to store all of the values a motor needs */
-    public static class TalonSRXConfig {
-        public final boolean INVERTED;
-        public final NeutralMode NEUTRAL_MODE;
-        public final int PEAK_CURRENT_LIMIT_AMPS;
-        public final double OPEN_LOOP_RAMP_RATE;
+    // public static class TalonSRXConfig {
+    //     public final boolean INVERTED;
+    //     public final NeutralModeValue NEUTRAL_MODE;
+    //     public final int PEAK_CURRENT_LIMIT_AMPS;
+    //     public final double OPEN_LOOP_RAMP_RATE;
 
-        public TalonSRXConfig(
-                boolean inverted,
-                NeutralMode neutralMode,
-                int peakCurrentLimitAmps,
-                double openLoopRampRate) {
-            this.INVERTED = inverted;
-            this.NEUTRAL_MODE = neutralMode;
-            this.PEAK_CURRENT_LIMIT_AMPS = peakCurrentLimitAmps;
-            this.OPEN_LOOP_RAMP_RATE = openLoopRampRate;
-        }
+    //     public TalonSRXConfig(
+    //             boolean inverted,
+    //             NeutralMode neutralMode,
+    //             int peakCurrentLimitAmps,
+    //             double openLoopRampRate) {
+    //         this.INVERTED = inverted;
+    //         this.NEUTRAL_MODE = neutralMode;
+    //         this.PEAK_CURRENT_LIMIT_AMPS = peakCurrentLimitAmps;
+    //         this.OPEN_LOOP_RAMP_RATE = openLoopRampRate;
+    //     }
 
-        public TalonSRXConfig(boolean inverted, NeutralMode neutralMode, int peakCurrentLimitAmps) {
-            this(inverted, neutralMode, peakCurrentLimitAmps, 0.0);
-        }
+    //     public TalonSRXConfig(boolean inverted, NeutralMode neutralMode, int peakCurrentLimitAmps) {
+    //         this(inverted, neutralMode, peakCurrentLimitAmps, 0.0);
+    //     }
 
-        public TalonSRXConfig(boolean inverted, NeutralMode neutralMode) {
-            this(inverted, neutralMode, 80);
-        }
+    //     public TalonSRXConfig(boolean inverted, NeutralMode neutralMode) {
+    //         this(inverted, neutralMode, 80);
+    //     }
 
-        public void configure(WPI_TalonSRX motor) {
-            motor.setInverted(INVERTED);
-            motor.setNeutralMode(NEUTRAL_MODE);
-            motor.configContinuousCurrentLimit(PEAK_CURRENT_LIMIT_AMPS - 10, 0);
-            motor.configPeakCurrentLimit(PEAK_CURRENT_LIMIT_AMPS, 0);
-            motor.configPeakCurrentDuration(100, 0);
-            motor.enableCurrentLimit(true);
-            motor.configOpenloopRamp(OPEN_LOOP_RAMP_RATE);
-        }
-    }
+    //     public void configure(WPI_TalonSRX motor) {
+    //         motor.setInverted(INVERTED);
+    //         motor.setNeutralMode(NEUTRAL_MODE);
+    //         motor.configContinuousCurrentLimit(PEAK_CURRENT_LIMIT_AMPS - 10, 0);
+    //         motor.configPeakCurrentLimit(PEAK_CURRENT_LIMIT_AMPS, 0);
+    //         motor.configPeakCurrentDuration(100, 0);
+    //         motor.enableCurrentLimit(true);
+    //         motor.configOpenloopRamp(OPEN_LOOP_RAMP_RATE);
+    //     }
+    // }
 
-    public static class VictorSPXConfig {
-        public final boolean INVERTED;
-        public final NeutralMode NEUTRAL_MODE;
-        public final double OPEN_LOOP_RAMP_RATE;
+    // public static class VictorSPXConfig {
+    //     public final boolean INVERTED;
+    //     public final NeutralMode NEUTRAL_MODE;
+    //     public final double OPEN_LOOP_RAMP_RATE;
 
-        public VictorSPXConfig(
-                boolean inverted,
-                NeutralMode neutralMode,
-                double openLoopRampRate) {
-            this.INVERTED = inverted;
-            this.NEUTRAL_MODE = neutralMode;
-            this.OPEN_LOOP_RAMP_RATE = openLoopRampRate;
-        }
+    //     public VictorSPXConfig(
+    //             boolean inverted,
+    //             NeutralMode neutralMode,
+    //             double openLoopRampRate) {
+    //         this.INVERTED = inverted;
+    //         this.NEUTRAL_MODE = neutralMode;
+    //         this.OPEN_LOOP_RAMP_RATE = openLoopRampRate;
+    //     }
 
-        public VictorSPXConfig(boolean inverted, NeutralMode neutralMode) {
-            this(inverted, neutralMode, 0.0);
-        }
+    //     public VictorSPXConfig(boolean inverted, NeutralMode neutralMode) {
+    //         this(inverted, neutralMode, 0.0);
+    //     }
 
-        public void configure(WPI_VictorSPX motor) {
-            motor.setInverted(INVERTED);
-            motor.setNeutralMode(NEUTRAL_MODE);
-            motor.configOpenloopRamp(OPEN_LOOP_RAMP_RATE);
-        }
-    }
+    //     public void configure(WPI_VictorSPX motor) {
+    //         motor.setInverted(INVERTED);
+    //         motor.setNeutralMode(NEUTRAL_MODE);
+    //         motor.configOpenloopRamp(OPEN_LOOP_RAMP_RATE);
+    //     }
+    // }
 }
