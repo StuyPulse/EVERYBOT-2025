b3928ab10805fce7d78fa3ee17af21b182f6328a
Danx3mer <dashingdaniel05@gmail.com>
2025-10-12 12:51:04 -0400

    Robo Replay day 2 lunch

--- src/main/java/com/stuypulse/robot/constants/Settings.java
+++ src/main/java/com/stuypulse/robot/constants/Settings.java
@@ -75,7 +75,7 @@ public interface Settings {
         public static SmartNumber ALGAE_OUTTAKE_SPEED = new SmartNumber("Pivot/Roller/Algae/Outtake Speed", 0.75); 
         public static SmartNumber ALGAE_HOLD_SPEED = new SmartNumber("Pivot/Roller/Algae/Hold Speed", -0.25); 
         
-        public static SmartNumber CORAL_SHOOT_SPEED = new SmartNumber("Pivot/Roller/Coral/Shoot Speed", -0.49);
+        public static SmartNumber CORAL_SHOOT_SPEED = new SmartNumber("Pivot/Roller/Coral/Shoot Speed", -0.47);
         public static SmartNumber ROLLER_ROTISSERIE_SPEED = new SmartNumber("Pivot/Roller/Coral/Hold Speed", 0.17);
         public static SmartNumber ROLLER_RESEAT_SPEED = new SmartNumber("Pivot/Roller/Coral/Reseat Speed", 0.7);
         
